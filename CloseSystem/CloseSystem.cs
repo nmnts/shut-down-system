61f7ebc5d9a2b508bfb1df8e3e38b32ebed58d56
namneet singh <namneet@revinfotech.com>
2017-12-07 14:49:50 +0530

    system-shutdown

---
+++ CloseSystem/CloseSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace CloseSystem
+{
+    public class CloseSystem
+    {
+        public static void CloseAllBrowser()
+        {
+            Process[] AllProcesses = Process.GetProcesses();
+            foreach (var process in AllProcesses)
+            {
+                if (process.MainWindowTitle != "")
+                {
+                    string s = process.ProcessName.ToLower();
+                    if (s == "iexplore" || s == "iexplorer" || s == "chrome" || s == "firefox")
+                        process.Kill();
+                }
+            }
+        }
+
+        public static void CloseAllFolders()
+        {
+            foreach (Process p in Process.GetProcessesByName("explorer"))
+            {
+                p.Kill();
+            }
+        }
+
+        public static void ShutDownSystem()
+        {
+            Process.Start("Shutdown", "-s -t 0");
+        }
+    }
+}
